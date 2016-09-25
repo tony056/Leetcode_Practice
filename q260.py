4bbc501611200a7238f85107fd0f011b4e50a0ef
Tony Tung <tonytung@D-10-157-165-64.dhcp4.washington.edu>
2016-09-24 23:13:00 -0700

    solving q260 with dynamic storage use

--- q260.py
+++ q260.py
@@ -1,11 +1,14 @@
 class Solution(object):
-	# """docstring for Solution"""
-	# def __init__(self, arg):
-	# 	super(Solution, self).__init__()
-	# 	self.arg = arg
 	def singleNumber(self, nums):
-
+		onceElement = []
+		for num in nums:
+			if num in onceElement:
+				index = onceElement.index(num)
+				del onceElement[index]
+			else:
+				onceElement.append(num)
+		return onceElement
 
 solution = Solution()
-print solution.singleNumber([])
+print solution.singleNumber([1, 2, 3, 4, 5, 1, 3, 2])
 		
