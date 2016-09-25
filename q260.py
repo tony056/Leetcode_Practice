bfd8915a068ea3ec69f75cbdc38c0b2a30b22724
Tony Tung <tonytung@D-10-157-165-64.dhcp4.washington.edu>
2016-09-24 21:46:40 -0700

    git init

---
+++ q260.py
@@ -0,0 +1,11 @@
+class Solution(object):
+	# """docstring for Solution"""
+	# def __init__(self, arg):
+	# 	super(Solution, self).__init__()
+	# 	self.arg = arg
+	def singleNumber(self, nums):
+
+
+solution = Solution()
+print solution.singleNumber([])
+		
